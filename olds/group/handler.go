8b86efd31a12f3847447ece620599c1d486699a5
loacl_wafer <wangzefeng2333@gmail.com>
2025-03-11 15:01:07 +0900

    add group function, change related

---
+++ olds/group/handler.go
@@ -0,0 +1,76 @@
+package group
+
+import (
+	"net/http"
+)
+
+// using context to segment resp and req
+type HandlerFunc func(*Context)
+
+// using router to replace map[string]HandlerFuncv1
+type Engine struct {
+	router *router
+
+	*RouterGroup
+	groups []*RouterGroup // store all groups
+}
+
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc // support middleware
+	parent      *RouterGroup  // support nesting
+	engine      *Engine       // all groups share a Engine instance
+}
+
+// New is the constructor of gee.Engine
+//
+//	func New() *Engine {
+//		return &Engine{router: newRouter()}
+//	}
+//
+// new 一定要改， 每一次修改后
+func New() *Engine {
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
+}
+
+// Group is defined to create a new RouterGroup
+// remember all groups share the same Engine instance
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+
+	group.engine.router.addRoute(method, pattern, handler)
+}
+
+// GET defines the method to add GET request
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
+}
+
+// Run defines the method to start a http server
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	c := newContext(w, req)
+	engine.router.handle(c)
+}
---
+++ olds/group/handler_test.go
@@ -0,0 +1,31 @@
+package group
+
+import (
+	"net/http"
+	"testing"
+)
+
+func Test_handler(t *testing.T) {
+
+	r := New()
+
+	r.GET("/", func(c *Context) {
+		c.SendHTMLResponse(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+
+	group := r.Group("/group")
+	group.GET("/hello", func(c *Context) {
+		// expect /group/hello?name=wafer
+		c.SendTextResponse(http.StatusOK, "hello %s, you're at %s\n", c.GetQueryParam("name"), c.Path)
+	})
+	group.GET("/hello/:name", func(c *Context) {
+		// expect /group/hello/wafer
+		c.SendTextResponse(http.StatusOK, "hello %s, you're at %s\n", c.GetDynamicParam("name"), c.Path)
+	})
+	group.GET("/assets/*filepath", func(c *Context) {
+		c.SendJSONResponse(http.StatusOK, Hash{"filepath": c.GetDynamicParam("filepath")})
+	})
+
+	r.Run("localhost:8080")
+
+}
